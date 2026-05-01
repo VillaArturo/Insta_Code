e34e0a4be4cccfaf9f300fd5504852c6f774c851
LuisAngelHernandezVargas215254 <al215254@alumnos.uacj.mx>
2026-04-30 20:36:24 -0600

    SCRUM-40 Parser y carga de archivos VB6 implementados

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
 #Filtro de datos de Cliente-Backend
-class TranslationSerializer(serializers.serializer):
+class TranslationSerializer(serializers.Serializer):
     source_code = serializers.CharField()
     language = serializers.CharField(default='vb6')
--- api/views.py
+++ api/views.py
@@ -1,34 +1,81 @@
 from django.shortcuts import render
 
+from api.parser.parser_vb6 import parsear_vb6
+from api.parser.limpiador import limpiar_codigo
+from api.parser.contexto import preparar_contexto
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import TranslationSerializer
 
 from api.ia.orquestador import OrquestadorIA
+from api.ia.cliente_mock import ClienteMock  # 👈 IMPORTANTE
 
 
 class TranslateCodeView(APIView):
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.orquestador = OrquestadorIA()
+        cliente = ClienteMock()  #crear cliente
+        self.orquestador = OrquestadorIA(cliente)
 
     def post(self, request):
-        serializer = TranslationSerializer(data=request.data)
+        
+        print("FILES:", request.FILES)
+        print("DATA:", request.data)
+        
+        file = request.FILES.get('file')
 
-        if serializer.is_valid():
-            # Datos correctos según tu serializer
+
+        if file:
+            if not file.name.endswith(('.bas', '.frm', '.cls')):
+                return Response(
+                    {"error": "Formato no válido"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+            try:
+                codigo_fuente = file.read().decode('utf-8', errors='ignore')
+                lenguaje = "vb6"
+            except:
+                return Response(
+                    {"error": "Error al leer archivo"},
+                    status=status.HTTP_400_BAD_REQUEST
+                )
+
+        else:
+            serializer = TranslationSerializer(data=request.data)
+
+            if not serializer.is_valid():
+                return Response(serializer.errors, status=400)
+
             codigo_fuente = serializer.validated_data.get("source_code")
             lenguaje = serializer.validated_data.get("language")
 
-            # Aquí entra la IA
-            resultado = self.orquestador.convertir_codigo(codigo_fuente)
+        #flijo del parser
+        codigo_limpio = limpiar_codigo(codigo_fuente)
+        estructura = parsear_vb6(codigo_limpio)
+        contexto = preparar_contexto(estructura)
 
-            return Response({
-                "status": "success",
-                "language": lenguaje,
-                "input": codigo_fuente,
-                "output": resultado["codigo_convertido"]
-            })
+        print("=== CODIGO LIMPIO ===")
+        print(codigo_limpio)
 
-        return Response(serializer.errors, status=400)
+        print("=== ESTRUCTURA ===")
+        print(estructura)
+
+        print("=== CONTEXTO ===")
+        print(contexto)
+        
+        #trabajo de IA
+        resultado = self.orquestador.convertir_codigo(contexto)
+
+        return Response({
+            "status": "success",
+            "language": lenguaje,
+            "input": codigo_fuente[:200],
+            "output": resultado["codigo_convertido"]
+        })
+        
+def home(request):
+    return render(request, 'index.html')
